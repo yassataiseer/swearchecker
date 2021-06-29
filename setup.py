7ec036d8c12e4b373cf3dc91012db164aa2c463f
Yassa Taiseer <yassataiseer@gmail.com>
2021-06-29 00:26:48 -0400

    commit

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
-  'Operating System :: Mac :: MacOS ::MacOS BigSur',
+  
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
  
 setup(
-  name='swearchecker',
+  name='cuschecker',
   version='0.0.1',
   description='Checks for swear words',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
